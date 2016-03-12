f414c28411cd339a828ffda6206d83df6e60a8a9
Matt Dang <mdang808@gmail.com>
2016-03-12 00:46:21 -0500

    starting to update to use mysqli commands

--- sign-up-form.php
+++ sign-up-form.php
@@ -16,6 +16,11 @@
   if (isset($_POST['totalTime']))
     $totalTime = $_POST['totalTime'];
 
+  function print_leaderboard() {
+    $top15_lb = new Leaderboard('Matt', 10);
+    return $top15_lb->display_leaderboards();
+  }
+
   class Leaderboard {
     private $name;
     private $totalTime;
@@ -32,34 +37,40 @@
      * Makes a connection with the database 
      */
     private function database_connect() {
-      $conn = mysql_connect('SERVER_NAME', 'USER', 'PASSWORD');  // replace the three fields
+      $conn = new mysqli('SERVER', 'USER', 'PASSWORD', 'DATABASE');
 
-      if (! $conn) {
-        throw new Exception('Database error: ' . mysql_error());
+      if ($conn->connect_errno > 0) {
+        die("Error in connecting to database: " . $conn->connect_error);
       }
 
-      mysql_select_db('DATABASE', $conn);  // replace DATABASE with database name
+      return $conn;
     }
 
     /*
      * Displays the top 15 fastest times
      */
     public function display_leaderboards() {
-      database_connect();  // makes the connection to the database
+      $conn = $this->database_connect();  // makes the connection to the database
 
-      $ordered_data = mysql_query("SELECT * FROM CRP3_leaderboards ORDER BY TIME ASC LIMIT 15");
+      $query = <<<SQL
+          SELECT * FROM CRP3_leaderboards
+          ORDER BY TIME ASC LIMIT 15;
+SQL;
 
-      if (! $ordered_data) {
-        throw new Exception("Error with MySQL query.");
+      if (!$result = $conn->query($query)) {
+        die("Error with query: " . $conn->error);
       }
 
       $leaderboards_table = '';
       $rank = 1;
 
-      while ($row_lb = mysql_fetch_assoc($ordered_data)) {
-        $leaderboards_table .= "\n<tr>\n<td>" . $rank . ".</td>\n<td>" . $row_lb['NAME'] . "</td>\n<td>" . $row_lb['TIME'] . "</tr>";
+      while ($row = $result->fetch_assoc()) {
+        $leaderboards_table .= "\n<tr>\n<td>" . $rank . ".</td>\n<td>" . $row['NAME'] . 
+            "</td>\n<td>" . $row['TIME'] . "</tr>";
         $rank += 1;
       }
+
+      $conn->close();
 
       return $leaderboards_table;
     }
@@ -68,10 +79,28 @@
      * Adds a record into the table
      */
     public function add_to_database() {
-      database_connect();
-      $result = mysql_query("SELECT NAME FROM CRP3_leaderboards");
+      $conn = $this->database_connect();  // makes the connection to the database
+      $names = $conn->query("SELECT NAME FROM CRP3_leaderboards");
+
+      if ($names === false) {
+        die("Error with names query: " . $conn->error);
+      }
+
+      $name_in_table = false;
+      $names->data_seek(0);
+      while($row = $names->fetch_row()) {
+        if ($row['NAME'] == $this->name) {  // name is already in table
+          $name_in_table = true;
+          return;  // exit function
+        }
+      }
+
       $id = mt_rand();
-      //$cmd = "INSERT INTO CRP3_leaderboards (ID, NAME, TIME)";
+
+      $insert = <<<SQL
+          INSERT INTO CRP3_leaderboards (ID, NAME, TIME) 
+          VALUES ($id $this->name $this->totalTime);
+SQL;
     }
   }
 ?>
@@ -99,7 +128,7 @@
           </tr>
         </thead>
         <tbody>
-          <?php echo display_leaderboards() ?>
+          <?php echo print_leaderboard() ?>
         </tbody>
       </table>
     </div>
